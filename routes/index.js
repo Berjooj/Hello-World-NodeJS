0fc2d5dca295af8239adfbf6bf547610631e8568
Berjooj <moreira.bernardo@yahoo.com.br>
2018-11-01 10:04:13 -0300

    Affe

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Olá mundo' });
+});
+
+/* GET new page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Cadastro' });
+});
+
+/* POST new page. */
+router.post('/new', function(req, res, next) {
+  var nome = req.body.nome;
+  var sobrenome = req.body.sobrenome;
+  res.redirect("/?nome=" + nome);
+});
+
+module.exports = router;
